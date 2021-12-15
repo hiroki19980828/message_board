c2310205cfa1cee04de41b131d6c34c56f59c166
hiroki.yamamoto <basket.hiroki0828@gmail.com>
2021-12-15 19:29:20 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -36,15 +36,20 @@ public class IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
-        
+
         em.close();
-        
+
         request.setAttribute("messages",messages);
-        
+     // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
 
-        
+
     }
 
 }
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -18,8 +18,8 @@ import utils.DBUtil;
  */
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,40 +28,41 @@ public class UpdateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String _token = request.getParameter("_token");
-		if(_token !=null && _token.equals(request.getSession().getId())) {
-		    EntityManager em = DBUtil.createEntityManager();
-		    
-		    //セッションスコープからメッセージIDを取得して
-		    //該当のIDのメッセージ一件のみをデータベースから取得
-		    Message m =em.find(Message.class,(Integer)(request.getSession().getAttribute("message_id")));
-		    
-		    String title =request.getParameter("title");
-		    m.setTitle(title);
-		    
-		    String content = request.getParameter("content");
-		    m.setContent(content);
-		    
-		    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-		    m.setUpdated_at(currentTime);     //更新日のみ上書き
-		    
-		    //データベースを更新
-		    em.getTransaction().begin();
-		    em.getTransaction().commit();
-		    em.close();
-		    
-		    //セッションスコープ上の不要になったデータの削除
-		    request.getSession().removeAttribute("message_id");
-		    
-		    //indexページへリダイレクト
-		    response.sendRedirect(request.getContextPath()+"/index");
-		    
-		   
-		}
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token !=null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            //セッションスコープからメッセージIDを取得して
+            //該当のIDのメッセージ一件のみをデータベースから取得
+            Message m =em.find(Message.class,(Integer)(request.getSession().getAttribute("message_id")));
+
+            String title =request.getParameter("title");
+            m.setTitle(title);
+
+            String content = request.getParameter("content");
+            m.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime);     //更新日のみ上書き
+
+            //データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "更新が完了しました。");       // ここを追記
+            em.close();
+
+            //セッションスコープ上の不要になったデータの削除
+            request.getSession().removeAttribute("message_id");
+
+            //indexページへリダイレクト
+            response.sendRedirect(request.getContextPath()+"/index");
+
+
+        }
+    }
 
 }
